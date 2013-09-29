96abe78d92c9f61ff57146e6e3a2af859da2f8a8
Colin Wood <cwood06@gmail.com>
2013-09-29 18:13:59 -0400

    Create some real tests.

--- tests.py
+++ tests.py
@@ -1,8 +1,25 @@
 import unittest
+import pyjolokia
 
 
 class CoreJolikiaTests(unittest.TestCase):
 
-    def test_travis(self):
+    def setUp(self):
+        self.client = pyjolokia.Jolokia('http://example.com/joloka/')
 
-        self.assertTrue(True)
+    def default_test_timeout(self):
+        """ Test default timeout """
+        self.assertEqual(self.client.timeout, 10)
+
+    def custom_test_timeout(self):
+        """ Test for custom timeout """
+        client = pyjolokia.Jolokia('http://example.com/jolokia/', timeout=20)
+        self.assertEqual(client.timeout, 20)
+
+    def basic_auth_test(self):
+        self.client.auth(httpusername='test', httppassword='testpassword')
+
+        self.assertEqual(self.client.authConfig['auth']['username'],
+                         'test')
+        self.assertEqual(self.client.authConfig['auth']['password'],
+                         'testpassword')
