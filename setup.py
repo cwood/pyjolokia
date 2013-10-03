b88f064ee7242d631a12ccb4e3f8af356cb867f6
Colin Wood <cwood06@gmail.com>
2013-10-02 21:58:04 -0400

    Update classifiers.

--- setup.py
+++ setup.py
@@ -31,5 +31,13 @@ setup(name='pyjolokia',
       include_package_data=True,
       keywords=['jolokia', 'jmx'],
       cmdclass = {'test': PyTest},
+      classifiers=[
+          'Development Status :: 4 - Beta'
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Topic :: Software Development :: Libraries :: Java Libraries',
+      ]
       **kw
 )
